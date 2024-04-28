a0506592157a197953da267541a6e8c4fe80c7ee
csaba <kavcsicsabcsi@gmail.com>
2024-04-29 01:41:42 +0200

    "client updated"

--- OneMessenger/Src/OneMessenger.Client/ClientCallBack.cs
+++ OneMessenger/Src/OneMessenger.Client/ClientCallBack.cs
@@ -8,7 +8,6 @@ namespace OneMessenger.Client
 	[CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
 	public class ClientCallback:IClient
 	{
-		
 		public void GetMessage(string username, string message)
 		{
 			((MainWindow)Application.Current.MainWindow).TakeMessage(username,message);
